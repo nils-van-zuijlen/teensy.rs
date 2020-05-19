e8f88305938f04c019410892ac90519f61a2d004
Nils VAN ZUIJLEN <nils.van-zuijlen@mailo.com>
2020-05-20 00:04:01 +0200

    Part 1
    https://branan.github.io/teensy/2017/01/12/bootup.html

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+#![feature(stdsimd)]
+#![no_std]
+#![no_main]
+
+mod port;
+mod sim;
+mod watchdog;
+
+#[no_mangle]
+pub extern fn main() {
+    let (wdog,sim,pin) = unsafe {
+        (watchdog::Watchdog::new(),
+         sim::Sim::new(),
+         port::Port::new(port::PortName::C).pin(5))
+    };
+
+    wdog.disable();
+    sim.enable_clock(sim::Clock::PortC);
+
+    let mut gpio = pin.make_gpio();
+
+    gpio.output();
+    gpio.high();
+
+    loop {}
+}
+
+#[panic_handler]
+fn teensy_panic(_pi: &core::panic::PanicInfo) -> ! {
+    loop {};
+}
+
+extern {
+    fn _stack_top();
+}
+
+#[link_section = ".vectors"]
+#[no_mangle]
+pub static _VECTORS: [unsafe extern fn(); 2] = [
+    _stack_top,
+    main,
+];
+
+#[link_section = ".flashconfig"]
+#[no_mangle]
+pub static _FLASHCONFIG: [u8; 16] = [
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+    0xFF, 0xFF, 0xFF, 0xFF, 0xDE, 0xF9, 0xFF, 0xFF
+];
